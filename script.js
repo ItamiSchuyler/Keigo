44095cebf8ca6e322b057fc77277c56675fd39c6
Itami <89473133+ItamiSchuyler@users.noreply.github.com>
2026-03-15 16:18:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,34 @@
-let player=document.getElementById("player")
+document.addEventListener("DOMContentLoaded",()=>{
 
-let title=document.getElementById("title")
+let player = document.getElementById("player")
+let title = document.getElementById("title")
 
 title.addEventListener("click",()=>{
-
 document.getElementById("about").classList.remove("hidden")
-
 })
 
-function enterSite(){
-
+window.enterSite = function(){
 document.getElementById("login").classList.add("hidden")
-
 document.getElementById("hero").classList.remove("hidden")
-
 }
 
-function closeAbout(){
-
+window.closeAbout = function(){
 document.getElementById("about").classList.add("hidden")
-
 }
 
-function toggleMusic(){
-
+window.toggleMusic = function(){
 if(player.paused){
-
 player.play()
-
 }else{
-
 player.pause()
-
 }
-
 }
-
-/* PARALLAX */
 
 document.addEventListener("mousemove",(e)=>{
-
 let x=e.clientX/40
-
 let y=e.clientY/40
-
 document.querySelector(".background").style.transform=
-
 "scale(1.1) translate("+x+"px,"+y+"px)"
+})
 
 })
