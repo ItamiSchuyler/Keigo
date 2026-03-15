8002850d7f1557e789a645bd7e328773383d2adc
Itami <89473133+ItamiSchuyler@users.noreply.github.com>
2026-03-15 15:34:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function enter(){
+let player=document.getElementById("player")
+
+let title=document.getElementById("title")
+
+title.addEventListener("click",()=>{
+
+document.getElementById("about").classList.remove("hidden")
+
+})
+
+function enterSite(){
 
 document.getElementById("login").classList.add("hidden")
 
@@ -6,14 +16,36 @@ document.getElementById("hero").classList.remove("hidden")
 
 }
 
-function openCard(){
+function closeAbout(){
 
-document.getElementById("card").classList.remove("hidden")
+document.getElementById("about").classList.add("hidden")
+
+}
+
+function toggleMusic(){
+
+if(player.paused){
+
+player.play()
+
+}else{
+
+player.pause()
 
 }
 
-function closeCard(){
-
-document.getElementById("card").classList.add("hidden")
-
 }
+
+/* PARALLAX */
+
+document.addEventListener("mousemove",(e)=>{
+
+let x=e.clientX/40
+
+let y=e.clientY/40
+
+document.querySelector(".background").style.transform=
+
+"scale(1.1) translate("+x+"px,"+y+"px)"
+
+})
