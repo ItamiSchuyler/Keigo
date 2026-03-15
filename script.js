b2b18c42ecd87c872bb85345f550fe00b85cf71c
Itami <89473133+ItamiSchuyler@users.noreply.github.com>
2026-03-15 15:24:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function enter(){
+
+document.getElementById("login").classList.add("hidden")
+
+document.getElementById("hero").classList.remove("hidden")
+
+}
+
+function openCard(){
+
+document.getElementById("card").classList.remove("hidden")
+
+}
+
+function closeCard(){
+
+document.getElementById("card").classList.add("hidden")
+
+}
