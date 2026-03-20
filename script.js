f8565084f3e3ce0d5d10e2ae88495ce1772a3a6c
Itami <89473133+ItamiSchuyler@users.noreply.github.com>
2026-03-20 14:43:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let player = document.getElementById("player")
 let title = document.getElementById("title")
 let bg = document.getElementById("bg")
 let fade = document.getElementById("fade")
+let glitch = document.getElementById("glitch")
+let character = document.querySelector(".character")
 
 /* ========= TRANSICION ANIME ========= */
 
@@ -24,14 +26,18 @@ fade.style.opacity = "0"
 }
 
 /* fondo inicial LOGIN */
-bg.style.backgroundImage = "url('https://i.postimg.cc/MpKDtbrF/wp13966102-shikamaru-pc-wallpapers.jpg')"
+
+bg.style.backgroundImage =
+"url('https://i.postimg.cc/MpKDtbrF/wp13966102-shikamaru-pc-wallpapers.jpg')"
 
 /* ========= EVENTOS ========= */
 
 title.addEventListener("click",()=>{
 document.getElementById("about").classList.remove("hidden")
 
-animeTransition("https://i.postimg.cc/L8bzDdPB/wp13966112-shikamaru-pc-wallpapers.jpg")
+animeTransition(
+"https://i.postimg.cc/L8bzDdPB/wp13966112-shikamaru-pc-wallpapers.jpg"
+)
 })
 
 window.enterSite = function(){
@@ -41,10 +47,22 @@ let p = document.getElementById("pass").value
 
 if(u === "seiren" && p === "stark"){
 
+/* GLITCH */
+
+glitch.style.opacity = "1"
+
+setTimeout(()=>{
+glitch.style.opacity = "0"
+},180)
+
+/* CAMBIO PANTALLA */
+
 document.getElementById("login").classList.add("hidden")
 document.getElementById("hero").classList.remove("hidden")
 
-animeTransition("https://i.postimg.cc/zGVhVdQm/wp13966252-shikamaru-pc-wallpapers.jpg")
+animeTransition(
+"https://i.postimg.cc/zGVhVdQm/wp13966252-shikamaru-pc-wallpapers.jpg"
+)
 
 }else{
 
@@ -61,9 +79,13 @@ box.classList.remove("shake")
 }
 
 window.closeAbout = function(){
+
 document.getElementById("about").classList.add("hidden")
 
-animeTransition("https://i.postimg.cc/zGVhVdQm/wp13966252-shikamaru-pc-wallpapers.jpg")
+animeTransition(
+"https://i.postimg.cc/zGVhVdQm/wp13966252-shikamaru-pc-wallpapers.jpg"
+)
+
 }
 
 window.toggleMusic = function(){
@@ -74,14 +96,35 @@ player.pause()
 }
 }
 
-/* ========= PARALLAX ========= */
+/* ========= PARALLAX + GLOW DINAMICO ========= */
 
 document.addEventListener("mousemove",(e)=>{
-let x = e.clientX/40
-let y = e.clientY/40
+
+let x = e.clientX / window.innerWidth
+let y = e.clientY / window.innerHeight
+
+/* PARALLAX FONDO */
+
+let moveX = (x - 0.5) * 25
+let moveY = (y - 0.5) * 25
 
 bg.style.transform =
-"scale(1.1) translate("+x+"px,"+y+"px)"
+"scale(1.12) translate("+moveX+"px,"+moveY+"px)"
+
+/* GLOW PERSONAJE */
+
+if(character){
+
+let glowX = (x - 0.5) * 60
+let glowY = (y - 0.5) * 60
+
+character.style.filter = `
+drop-shadow(${glowX}px ${glowY}px 40px var(--neonStrong))
+drop-shadow(0 0 25px var(--neonSoft))
+`
+
+}
+
 })
 
 })
