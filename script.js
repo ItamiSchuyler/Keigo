c01d1cecef372bb0f5ea566d8283e85b0d3eb00b
Itami <89473133+ItamiSchuyler@users.noreply.github.com>
2026-03-23 08:58:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,21 +12,49 @@ const pass = document.getElementById("pass")
 
 const player = document.getElementById("player")
 
-function enterSite(){
+const bg = document.getElementById("bg")
 
-if(user.value==="seiren" && pass.value==="stark"){
+/* BACKGROUNDS */
+
+const backgrounds={
+
+login:"https://i.postimg.cc/rpJ60qVQ/87a1d82fb5bc9d1e9baee8d72739b656.gif",
+
+hero:"https://i.postimg.cc/L6y5Y6qK/1665.gif",
+
+about:"https://i.postimg.cc/DZx8pKQm/tumblr-6baf.gif"
+
+}
+
+function changeBG(name){
 
 fade.style.opacity="1"
 
 setTimeout(()=>{
 
-login.classList.add("hidden")
-
-hero.classList.remove("hidden")
+bg.style.backgroundImage=`url(${backgrounds[name]})`
 
 fade.style.opacity="0"
 
-},500)
+},300)
+
+}
+
+/* LOGIN */
+
+function enterSite(){
+
+if(user.value==="admin" && pass.value==="admin"){
+
+changeBG("hero")
+
+login.classList.add("hidden")
+
+setTimeout(()=>{
+
+hero.classList.remove("hidden")
+
+},300)
 
 }else{
 
@@ -42,7 +70,11 @@ login.classList.remove("shake")
 
 }
 
+/* ABOUT */
+
 function openAbout(){
+
+changeBG("about")
 
 about.classList.remove("hidden")
 
@@ -50,9 +82,13 @@ about.classList.remove("hidden")
 
 function closeAbout(){
 
+changeBG("hero")
+
 about.classList.add("hidden")
 
 }
+
+/* MUSIC */
 
 function toggleMusic(){
 
@@ -67,3 +103,7 @@ player.pause()
 }
 
 }
+
+/* INITIAL BG */
+
+changeBG("login")
